b1303653433b7b96768579a57a8b99557d9dbcd9
ericknkunda <macnkunda@gmail.com>
2023-06-19 16:39:11 +0300

    this commit

--- app/src/main/java/com/example/test_resource_files/MainActivity.kt
+++ app/src/main/java/com/example/test_resource_files/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.test_resource_files
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 //    var myButton:Button=Button(this)
@@ -12,10 +17,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_resource)
 //        myButton =(Button)
+    var framelayout =findViewById<FrameLayout>(R.id.fragment_frame)
     }
 
     fun openActivitryTwo(view: View){
         val intent =Intent(this,activity2::class.java)
         startActivity(intent)
     }
+
+    fun openFirstFragment(view: View){
+        val fragmentInstance =eacMessage()
+        val layout =R.id.fragment_frame
+        openFragmentOne(fragmentInstance, layout)
+    }
+
+    fun openSecondFragment(view: View){
+        val fragment =ClickMeFragment()
+        val layoutId =R.id.fragment_frame
+
+        loadFragmentTwo(fragment, layoutId)
+    }
+    fun openFragmentOne(fragment:Fragment, layoutId:Int){
+        var manager: FragmentManager =supportFragmentManager
+        var transaction: FragmentTransaction =manager.beginTransaction()
+        transaction.replace(layoutId, fragment)
+        transaction.commit()
+
+    }
+
+    fun loadFragmentTwo(fragment: Fragment, layoutId: Int){
+      var  manager:androidx.fragment.app.FragmentManager = supportFragmentManager
+        var transaction:androidx.fragment.app.FragmentTransaction =manager.beginTransaction()
+        transaction.replace(layoutId, fragment)
+        transaction.commit()
+    }
+
+
 }
